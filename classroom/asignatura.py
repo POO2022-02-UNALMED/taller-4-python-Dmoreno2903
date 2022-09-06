1199c8254d08ecdb5eeef04600ec9ecfbf68beed
Juan Diego Aguirre Moreno <jaguirremo@unal.edu.co>
2022-09-05 22:50:55 -0500

    Test

--- classroom/asignatura.py
+++ classroom/asignatura.py
@@ -3,3 +3,6 @@ class Asignatura:
     def __init__(self, nombre, salon="remoto"):
         self._nombre = nombre
         self._salon = salon
+    def __repr__(self):
+        ret = self._nombre +' '+self._salon
+        return ret
--- classroom/grupo.py
+++ classroom/grupo.py
@@ -1,37 +1,41 @@
 
 
-from classroom.asignatura import Asignatura
-from classroom.asignatura import Asignatura
+from Taller4.asignatura import Asignatura
+from Taller4.asignatura import Asignatura
 
 
 class Grupo:
 
-    grado = None
+    grado = "Grado 12"
 
-    def __init__(self, grupo="grupo ordinado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
         self._grupo = grupo
         self._asignaturas = asignaturas
         self.listadoAlumnos = estudiantes
 
-    def listadoAsignaturas(self, kwargs):
-        for x in kwargs.values():
-            self._asignaturas.append(Asignatura(x))
+    def listadoAsignaturas(self, **kwargs):
+        values = kwargs.values()
+        self._asignaturas = values
 
-    def agregarAlumno(self, alumno, lista=[]):
+    def agregarAlumno(self, alumno, lista=None):
         if(lista is None):
-            lista.append(alumno)
-            self.listadoAlumnos = self.listadoAlumnos + lista
-        else:
             self.listadoAlumnos = [alumno]
+        else:
+            lista.append(alumno)
+            self.listadoAlumnos = lista
 
     @ classmethod
     def asignarNombre(cls, nombre="Grado 10"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 6"):
+    def asignarNombre(cls, nombre="Grado 4"):
         cls.grado = nombre
 
     @ classmethod
-    def asignarNombre(cls, nombre="Grado 4"):
+    def asignarNombre(cls, nombre="Grado 6"):
         cls.grado = nombre
+
+    def __repr__(self):
+        ret = "Grupo de estudiantes: "+self._grupo
+        return ret
